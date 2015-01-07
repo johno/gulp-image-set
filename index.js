33ce491cb763a98079abfd7dda605de624519c90
John Otander <johnotander@gmail.com>
2015-01-06 18:22:58 -0700

    Implement basic functionality

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 'use strict';
 
-module.exports = function gulpImageSet(options) {
-  options = options || {};
+var gutil = require('gulp-util');
+var through = require('through2');
+var rework = require('rework');
+var imageSet = require('rework-image-set');
 
-  return true;
+module.exports = function gulpImageSet() {
+  return through.obj(function(file, encoding, callback) {
+    if (file.isNull()) {
+      callback(null, file);
+    }
+
+    if (file.isStream()) {
+      callback(new gutil.PluginError('gulp-image-set', 'Streaming not supported'));
+      return;
+    }
+
+    try {
+      var inputCss = file.contents.toString();
+      var outputCss = rework(inputCss, { source: file.path }).use(imageSet()).toString({ sourcemap: true });
+
+      file.contents = new Buffer(outputCss);
+      this.push(file);
+    } catch (err) {
+      this.emit('error', new gutil.PluginError('gulp-image-set', err, { fileName: file.path }));
+    }
+
+    callback();
+  });
 }
--- test/test.js
+++ test/test.js
@@ -1,9 +1,31 @@
 var assert = require('assert');
+var gutil = require('gulp-util');
 var gulpImageSet = require('..');
 
 describe('gulp-image-set', function() {
 
-  it('should do something awesome', function() {
-    assert.equal(camelizedPackageName(), true);
+  it('should provide the correct css', function(done) {
+    var gulpImageSetStream = gulpImageSet();
+
+    var actual = '.bg-img {background-image: image-set(\'my-img.png\' 1x,"my-img-2x.png" 2x,\'my-img-print.png\' 600dpi);}.some-other-class {color: #fafafa;}';
+    var expected = '.bg-img {\n  background-image: url(\'my-img.png\');\n  background-image: image-set(\'my-img.png\' 1x,\'my-img-2x.png\' 2x,\'my-img-print.png\' 600dpi);\n}\n\n.some-other-class {\n  color: #fafafa;\n}\n';
+
+    gulpImageSetStream.once('data', function(file) {
+      assert.equal(file.relative, 'default.css');
+      assert.equal(withoutSourceMap(file.contents.toString()), expected);
+    });
+
+    gulpImageSetStream.on('end', done);
+
+    gulpImageSetStream.write(new gutil.File({
+      path: 'default.css',
+      contents: new Buffer(actual)
+    }));
+
+    gulpImageSetStream.end();
   });
 });
+
+function withoutSourceMap(str) {
+  return (str || '').split('/*# sourceMappingURL')[0];
+}
